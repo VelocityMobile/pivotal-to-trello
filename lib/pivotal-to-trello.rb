aac49827d07b9b64cb58ea342fb6e21851b98eda
Kir Shatrov <shatrov@me.com>
2015-02-16 10:44:34 +0200

    Simplify includes

--- lib/pivotal-to-trello.rb
+++ lib/pivotal-to-trello.rb
@@ -1,14 +1,4 @@
-unless defined? P2T_ROOT
-  $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
-  P2T_ROOT   = File.expand_path(File.dirname(__FILE__) + '/../')
-  P2T_LIB    = File.join(P2T_ROOT, 'lib', 'pivotal_to_trello')
-end
-
 module PivotalToTrello
-  def self.version
-    File.exist?(File.join(P2T_ROOT, 'VERSION')) ? File.read(File.join(P2T_ROOT, 'VERSION')) : 'Unknown'
-  end
-
   require 'pivotal_to_trello/core'
   require 'pivotal_to_trello/pivotal_wrapper'
   require 'pivotal_to_trello/runner'
